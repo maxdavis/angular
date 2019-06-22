3574b74fdf990a72263b9ad0f7e38dd54a24dc71
Max Davis Lins <maxdavisdesign@gmail.com>
2019-06-22 12:35:26 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -18,4 +18,12 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json()).catch(ErrorHandler.handlerError)
     }
+
+    restaurantsById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handlerError)
+    }
+
+
 }
